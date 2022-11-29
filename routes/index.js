8b54e03d8ed6f53c9159cdf7f08ee63ca22afd07
sinhay <62039981+sinhay@users.noreply.github.com>
2022-11-28 20:49:00 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ const userRoutes = require('./users');
 const constructorMethod = (app) => {
     // figure out what route base should be stored in
     app.use('/', apiRoutes);
+    app.use('/register', apiRoutes);
+    app.use('/login', apiRoutes);
+    app.use('/logout', apiRoutes);
+    app.use('/protected', apiRoutes);
     app.use('/playlists', playlistRoutes);
     app.use('/song', songRoutes);
     app.use('/user', userRoutes);
