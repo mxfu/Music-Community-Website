daf74c87da99e8ea3df15f72fb76df3a554065d9
sinhay <62039981+sinhay@users.noreply.github.com>
2022-11-28 20:51:12 -0500

    added bcrypt middleware declaration to data functions

--- data/songs.js
+++ data/songs.js
@@ -3,6 +3,10 @@ const mongoCollections = require('../config/mongoCollections');
 const songs = mongoCollections.songs;
 const {ObjectId} = require('mongodb');
 const helper = require('../helpers');
+const bcrypt = require('bcrypt');
+const saltRounds = 16;
+
+// data functions for songs
 
 module.exports = {
     
--- data/users.js
+++ data/users.js
@@ -3,6 +3,10 @@ const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
 const {ObjectId} = require('mongodb');
 const helper = require('../helpers');
+const bcrypt = require('bcrypt');
+const saltRounds = 16;
+
+// data functions for users
 
 module.exports = {
     
