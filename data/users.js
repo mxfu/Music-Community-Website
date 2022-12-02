f298ba2e8314d479adba04f4f01a621e7457552f
mxfu <mphu@stevens.edu>
2022-12-02 18:08:28 -0500

    checkUser

--- data/users.js
+++ data/users.js
@@ -1,13 +1,38 @@
 // functions for users
 const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
-const {ObjectId} = require('mongodb');
+const { ObjectId } = require('mongodb');
 const helper = require('../helpers');
 const bcrypt = require('bcrypt');
 const saltRounds = 16;
+const validation = require('../helpers');
 
 // data functions for users
+const createUser = async (username, password) => {
+
+}
+
+const checkUser = async (username, password) => {
+    username = validation.checkUsername(username);
+    password = validation.checkPassword(password);
+
+    //actual creation of user into db
+    const userCollection = await users();
+
+    let found = await userCollection.findOne({ username: username.toLowerCase() });
+    //checking for duplicates
+    if (!(found)) {
+        throw "user does not exist";
+    }
+
+    if (!(await bcrypt.compare(password.trim(), found.password))) {
+        throw "Either the username or password is invalid";
+    }
+
+    return { authenticatedUser: true };
+}
 
 module.exports = {
-    
+    createUser,
+    checkUser
 };
--- helpers.js
+++ helpers.js
@@ -1,5 +1,59 @@
 // Helper functions to be used in other files
+const { ObjectId } = require('mongodb');
 
 module.exports = {
-    
+    /*
+        username --> 5 characters, non-empty, no spaces, alphanumeric, no special characters
+        password --> 6 characters, non-empty, no spaces, 1 capital letter, lowercase, uppercase, and special character
+    */
+    checkUsername(username) {
+        //check for existence of arguments
+        if ((!username || !password)) {
+            throw "must provide both a username and password";
+        }
+
+        if (typeof username !== "string" || typeof password !== "string") {
+            throw "username and password must be strings";
+        }
+
+        //check username for letters and numbers only 
+        let unRegex = /^[a-zA-Z0-9]+$/;
+        let verify = true;
+
+        if (username.trim().match(unRegex) === null) {
+            verify = false;
+        }
+
+        if (verify === false) {
+            throw "username must be letters only";
+        }
+
+        //check username be at least 4 characters
+        if (username.trim().length < 5) {
+            throw "username must be at least 5 characters"
+        }
+
+        return username;
+
+    },
+
+    checkPassword(password) {
+        //check password for any character
+        let pwRegex = /^(?=.*?[A-Z])(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{6,}$/gm
+
+        if (password.trim().match(pwRegex) === null) {
+            verify = false;
+        }
+
+        if (verify === false) {
+            throw "password must have at least 1 uppercase letter, 1 number, and 1 special character";
+        }
+
+        //check password is at least 6 characters
+        if (password.trim().length < 6) {
+            throw "password must be at least 6 characters"
+        }
+
+        return password;
+    }
 };
