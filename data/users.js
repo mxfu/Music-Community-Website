895b19773fc1993e9ec94b3379ccb95f2cdab2d4
Leonel Trubbo <54546994+argentinegeek@users.noreply.github.com>
2022-12-02 17:56:16 -0500

    Started on createUser & some helper functions

--- data/users.js
+++ data/users.js
@@ -1,13 +1,40 @@
 // functions for users
-const mongoCollections = require('../config/mongoCollections');
+const mongoCollections = require("../config/mongoCollections");
 const users = mongoCollections.users;
-const {ObjectId} = require('mongodb');
-const helper = require('../helpers');
-const bcrypt = require('bcrypt');
+const { ObjectId } = require("mongodb");
+const helper = require("../helpers");
+const bcrypt = require("bcrypt");
 const saltRounds = 16;
 
 // data functions for users
 
+const createUser = async (
+  firstName,
+  lastName,
+  userName,
+  password,
+  confirmPassword
+) => {
+  helper.checkNames(firstName);
+  helper.checkNames(lastName);
+  helper.checkString(firstName);
+  helper.checkString(lastName);
+  helper.checkUsername(userName);
+  if (!password === confirmPassword)
+    throw "Error: password must match confirmPassword";
+  const userCollection = await users();
+  const hash = await bcrypt.hash(password, saltRounds);
+  let newUser = {
+    firstName: firstName,
+    lastName: lastName,
+    userName: userName,
+    password: hash,
+  };
+  const newInsert = await userCollection.insertOne(newUser);
+  if (newInsert.insertedCount === 0) throw "Insert failed!";
+  //return await this.getUserById(newInsert.insertedId.toString())
+};
+
 module.exports = {
-    
+  createUser,
 };
--- helpers.js
+++ helpers.js
@@ -1,5 +1,30 @@
 // Helper functions to be used in other files
-
-module.exports = {
-    
-};
+function checkNames(input) {
+  var namesCheck = "[a-zA-Z]+"; //checks for letters only
+  if (!namesCheck.test(input))
+    throw "Error: names must only contain alphabetical characters!";
+}
+function checkString(strVal, varName) {
+  if (!strVal) throw `Error: You must supply a ${varName}!`;
+  if (typeof strVal !== "string") throw `Error: ${varName} must be a string!`;
+  strVal = strVal.trim();
+  if (strVal.length === 0)
+    throw `Error: ${varName} cannot be an empty string or string with just spaces`;
+  if (!isNaN(strVal))
+    throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
+  return strVal;
+}
+function checkUsername(username) {
+  if (!username) throw "Error: username was not provided!";
+  if (typeof username !== "string")
+    throw "Error: Username is not of type string!";
+  username = username.trim();
+  if (username.length === 0) throw "Error: username cannot contain just spaces";
+  if (username.length <= 4)
+    throw "Error: username cannot be less than 5 characters";
+  const letters = /[a-zA-Z]/;
+  const letterCheck = letters.test(username);
+  if (!letterCheck)
+    throw "Error: Username must contain at least one letter character.";
+}
+module.exports = { checkNames, checkString, checkUsername };
